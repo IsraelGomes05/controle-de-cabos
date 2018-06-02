b020af7c5fb059ed08f9465d3f64554e58a2155e
Israel Gomes <israelgomes05@gmail.com>
2018-06-01 22:45:10 -0400

    Adicionando comentários a classe

--- src/br/com/sisnet/controledecabos/classes/utilitarias/Calculo.java
+++ src/br/com/sisnet/controledecabos/classes/utilitarias/Calculo.java
@@ -7,17 +7,21 @@ package br.com.sisnet.controledecabos.classes.utilitarias;
 import java.util.List;
 
 /**
- * @author Israel Gomes Da Silva
- * @contact israelgomes05@gmail.com
- * @created 27/03/2018
- * @lastModified 20/04/2018
- * @version 1.0
- *
- * @Function Classe utilizada para calcular números válidos para registros de
- * pontas e bobinas.
+ * Classe utilitária para calculo.
+ * @author Israel Gomes
+ * @version 2.0
+ * @since 1.0
  */
 public class Calculo {
 
+    /**
+     * Apartir da lista passada como parâmetro, é realizada uma iteração na mesma,
+     * buscando um número qualquer, que não esteja presente na respectiva lista, quando 
+     * encontrado este número é retornado.
+     * 
+     * @param list Integer - Lista preenchida com números inteiros.
+     * @return numValido int - Número que não está presente na lista 
+     */
     public static int NumValido(List<Integer> list) {
         int numValido = 0;
 
